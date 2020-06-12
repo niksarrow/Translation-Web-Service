ff8a019a76e14d07bb07f9ac3f688b25cedfacc3
Nikhil <niksarrow196@gmsil.com>
2020-06-12 17:59:28 +0530

    disfluency translating

--- translation/views.py
+++ translation/views.py
@@ -52,12 +52,12 @@ def translate(request):
     c = request.POST['source_sent']
     cmd = r"../seminar/iwslt_2020/"
     # print(os.getcwd())
-    try:
-        os.chdir(cmd)
-    except Error as e:
-        # os.chdir("-")
-        pass
+    os.chdir(cmd)
     # print(os.getcwd())
+    os.chdir("web_service")
+    cmd = 'echo "{0}" > test.es'.format(c)
+    os.system(cmd)
+    os.chdir("../")
     cmd = r"./get_data_esen_noise_web_service.sh"
     try:
         check_output(cmd, shell=True, stderr=subprocess.STDOUT)
@@ -66,15 +66,8 @@ def translate(request):
         raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
     # cwd = os.getcwd()
     # print(cwd)
-    os.chdir("web_service")
-    cmd = r'echo {0} > test.es'.format(c)
-    try:
-        check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
         
-    os.chdir("../")
-    cmd = r"python3 main.py --exp_name 'webservice' --transformer 'True' --n_enc_layers '4' --n_dec_layers '4' --share_enc '3' --share_dec '3' --share_lang_emb 'True' --share_output_emb 'True' --emb_dim '512' --langs 'en,es'  --para_dataset 'en-es:./web_service/train.XX.50000.pth,./web_service/tun.XX.50000.pth,./web_service/test.XX.50000.pth' --para_directions 'en-es,es-en' --n_para '-1' --lambda_xe_para '1' --pretrained_emb './data/noise1/mono/all.es-en.50000.vec' --dropout '0.3' --label-smoothing '0.1' --eval_only 'True' --reload_model './web_service/iwslt_mid.pth' --reload_enc 'True' --reload_dec 'True' --group_by_size 'False' --is_web_service 'True'"
+    cmd = r"python3 main.py --exp_name 'webservice' --transformer 'True' --n_enc_layers '4' --n_dec_layers '4' --share_enc '3' --share_dec '3' --share_lang_emb 'True' --share_output_emb 'True' --emb_dim '512' --langs 'en,es'  --para_dataset 'en-es:./web_service/train.XX.tok.50000.pth,./web_service/tun.XX.tok.50000.pth,./web_service/test.XX.tok.50000.pth' --para_directions 'en-es,es-en' --n_para '-1' --lambda_xe_para '1' --pretrained_emb './data/noise1/mono/all.es-en.50000.vec' --dropout '0.3' --label-smoothing '0.1' --eval_only 'True' --reload_model './web_service/iwslt_mid.pth' --reload_enc 'True' --reload_dec 'True' --group_by_size 'False' --is_web_service 'True'"
     try:
         check_output(cmd, shell=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
