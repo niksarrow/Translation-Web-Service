e95f4791ec0495fa574b3ab0a06ec84358d9b00f
Nikhil Saini <niksarrow196@gmail.com>
2020-06-12 19:25:46 +0530

    unmt

--- translation/views.py
+++ translation/views.py
@@ -51,9 +51,7 @@ def deleteUnmt(request, unmt_id):
 def translate(request):
     c = request.POST['source_sent']
     cmd = r"../seminar/iwslt_2020/"
-    # print(os.getcwd())
     os.chdir(cmd)
-    # print(os.getcwd())
     os.chdir("web_service")
     cmd = 'echo "{0}" > test.es'.format(c)
     os.system(cmd)
@@ -62,11 +60,8 @@ def translate(request):
     try:
         check_output(cmd, shell=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
-        #os.chdir("-")
         raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
-    # cwd = os.getcwd()
-    # print(cwd)
-        
+
     cmd = r"python3 main.py --exp_name 'webservice' --transformer 'True' --n_enc_layers '4' --n_dec_layers '4' --share_enc '3' --share_dec '3' --share_lang_emb 'True' --share_output_emb 'True' --emb_dim '512' --langs 'en,es'  --para_dataset 'en-es:./web_service/train.XX.tok.50000.pth,./web_service/tun.XX.tok.50000.pth,./web_service/test.XX.tok.50000.pth' --para_directions 'en-es,es-en' --n_para '-1' --lambda_xe_para '1' --pretrained_emb './data/noise1/mono/all.es-en.50000.vec' --dropout '0.3' --label-smoothing '0.1' --eval_only 'True' --reload_model './web_service/iwslt_mid.pth' --reload_enc 'True' --reload_dec 'True' --group_by_size 'False' --is_web_service 'True'"
     try:
         check_output(cmd, shell=True, stderr=subprocess.STDOUT)
@@ -84,8 +79,28 @@ def translate(request):
 
 def translateUnmt(request):
     c = request.POST['source_sent_unmt']
-    gen = c + " unmt"
-    # generate here
+    cmd = r"../seminar/iwslt_2020/"
+    os.chdir(cmd)
+    os.chdir("web_service")
+    cmd = 'echo "{0}" > test.es'.format(c)
+    os.system(cmd)
+    os.chdir("../")
+    cmd = r"./get_data_esen_noise_web_service.sh"
+    try:
+        check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+    cmd = r"python3 main.py --exp_name 'webservice' --transformer 'True' --n_enc_layers '4' --n_dec_layers '4' --share_enc '3' --share_dec '3' --share_lang_emb 'True' --share_output_emb 'True' --emb_dim '512' --langs 'en,es'  --para_dataset 'en-es:./web_service/train.XX.tok.50000.pth,./web_service/tun.XX.tok.50000.pth,./web_service/test.XX.tok.50000.pth' --para_directions 'en-es,es-en' --n_para '-1' --lambda_xe_para '1' --pretrained_emb './data/noise1/mono/all.es-en.50000.vec' --dropout '0.3' --label-smoothing '0.1' --eval_only 'True' --reload_model './web_service/iwslt_mid.pth' --reload_enc 'True' --reload_dec 'True' --group_by_size 'False' --is_web_service 'True'"
+    try:
+        check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+    os.chdir("web_service")
+    gen = open('gen.txt', 'r').readlines()[0]
+    os.chdir("../../../Translation-Web-Service")
+
     new_item = UnmtItem(content = c, gen = gen)
     new_item.save()
     all_todo_items = TodoItem.objects.all()
